dc3a6f464237b6059a08ce115e4613731547dd71
fenghuan <1207018406@qq.com>
2016-03-02 19:45:26 +0800

    修改样式

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,3 +1,17 @@
 /**
  * Created by lele88lala on 16/3/1.
  */
+$(document).ready(function(){
+    $(".siteNavRClassHasTwo").each(function(){
+        $(".siteNavRClassHasTwo").mouseover(function(){
+            $("a[id="+this.getAttribute("name")+"]").addClass('siteNavAHover');
+            $("div[name="+this.id+"]").show();
+            $(".siteNavRClassHasTwo").mouseout(function(){
+                $("a[id="+this.getAttribute("name")+"]").removeClass('siteNavAHover');
+
+                $("div[name="+this.id+"]").hide();
+            });
+        });
+
+    });
+});
